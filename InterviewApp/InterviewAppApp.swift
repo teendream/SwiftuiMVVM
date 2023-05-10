23bf39346f4bd884a1801ecc7ac08cd724da8d04
Azizbek <ourminedev@gmail.com>
2023-05-10 15:54:02 +0500

    Initial Commit

---
+++ InterviewApp/InterviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAppApp.swift
+//  InterviewApp
+//
+//  Created by Azizbek Abdulkhakimov on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
